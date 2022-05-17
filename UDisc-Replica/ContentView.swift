36dfa82ac2e79228711a60c7e377db49c6a4e45b
DJKramer314 <djkramer314@gmail.com>
2022-05-17 18:21:19 -0500

    Put the current header code into its own file

--- UDisc-Replica/ContentView.swift
+++ UDisc-Replica/ContentView.swift
@@ -11,24 +11,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Header()
-                .frame(height:100)
             Spacer()
         }
         .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct Header: View {
-    var body: some View {
-        ZStack {
-            Color(.darkGray)
-            Image("udisclogo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
-        }
-    }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ UDisc-Replica/Header.swift
@@ -0,0 +1,21 @@
+//
+//  Header.swift
+//  UDisc-Replica
+//
+//  Created by Dylan on 5/17/22.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        ZStack {
+            Color(.darkGray)
+            Image("udisclogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+        }
+        .frame(height:100)
+    }
+}
