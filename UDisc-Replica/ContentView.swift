6d836343d844a1c1e92ce92f3d8731913de964e0
Dylan Kramer <DJKramer314@gmail.com>
2022-05-17 17:20:14 -0500

    Initial Commit

---
+++ UDisc-Replica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UDisc-Replica
+//
+//  Created by Dylan on 5/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, GitHub!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UDisc-Replica/UDisc_ReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDisc_ReplicaApp.swift
+//  UDisc-Replica
+//
+//  Created by Dylan on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct UDisc_ReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
