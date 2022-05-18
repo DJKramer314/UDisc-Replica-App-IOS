55c49e4e3a2cb74794570597e866f6f16a276787
DJKramer314 <djkramer314@gmail.com>
2022-05-18 10:59:00 -0500

    Made a basic form that contains the first element on the page

--- UDisc-Replica/MoreView.swift
+++ UDisc-Replica/MoreView.swift
@@ -11,6 +11,17 @@ struct MoreView: View {
     var body: some View {
         VStack {
             Header()
+            
+            NavigationView {
+                Form {
+                    Section(header: Text("Play")) {
+                        NavigationLink(destination: Header()) {
+                            Text("Scorecards")
+                        }
+                    }
+                }
+            }
+            
             Spacer()
         }
     }
