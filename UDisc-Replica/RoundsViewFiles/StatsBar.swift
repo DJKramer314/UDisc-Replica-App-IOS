7dd17dad826c1cd6eb6f2047b64f3f9452e9d406
DJKramer314 <djkramer314@gmail.com>
2022-05-20 11:26:34 -0500

    This is a working version of the "Last Played" section

--- UDisc-Replica/RoundsViewFiles/StatsBar.swift
+++ UDisc-Replica/RoundsViewFiles/StatsBar.swift
@@ -18,14 +18,40 @@ struct StatsBar: View {
                 counter += 1
             }
         }
-        print(counter)
         return counter
     }
     
+    func computeLastPlayedRound(recentRounds: [Card]) -> String {
+        
+        if recentRounds.count == 0 {
+            return "Never"
+        }
+        
+        //This is a dummy round that is several years old, so any recent activity will be caught before this
+        var mostRecentRound: Card = Card(id: 0, title: "Placeholder Card", layout: "Placeholder Layout", date: Date(timeIntervalSinceNow: -999999999), user: "Placeholder User", score: "No Score")
+        
+        //Cycles through the rounds and finds the most recent one
+        for scorecard in recentRounds {
+            if scorecard.date.timeIntervalSinceNow > mostRecentRound.date.timeIntervalSinceNow {
+                if scorecard.date.timeIntervalSinceNow > -86400 { //One day
+                    return "Today"
+                } else if scorecard.date.timeIntervalSinceNow > -172800 { //Two days
+                    return "Yesterday"
+                }
+                mostRecentRound = scorecard
+            }
+        }
+        
+        //Calculates how many days ago the most recent round was
+        return "\(Int(floor((mostRecentRound.date.timeIntervalSinceNow / -86400)))) Days ago"
+    }
+    
+    
+    //Start of the view
     var body: some View {
         let total_rounds = appData.userRounds.count
         let recent_rounds = computeRecentRounds(recentRounds: appData.userRounds)
-        let last_played = "Today"
+        let last_played = computeLastPlayedRound(recentRounds: appData.userRounds)
         
         HStack {
             VStack {
