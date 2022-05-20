5398a9e72979cb2780ecb5969b84c0eab478a6e1
DJKramer314 <djkramer314@gmail.com>
2022-05-19 22:37:28 -0500

    Made the create scorecard button functional with immediate dummy data instead of a GUI

--- UDisc-Replica/RoundsViewFiles/CreateScorecardButtonView.swift
+++ UDisc-Replica/RoundsViewFiles/CreateScorecardButtonView.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+
+
+
 struct CreateScorecardButtonView: View {
+    
+    @EnvironmentObject var appData: AppData
+    
+    private func createScoreCard() {
+        appData.userRounds.append(Card(id: appData.userRounds.count, title: "Home Course", layout: "Main 9", date: "Today", user: appData.user, score: "0 (27)"))
+    }
+        
     var body: some View {
         ZStack {
             Color.gray
-            Button(action:{print("Hello")}) {
+            Button(action:{createScoreCard()}) {
                 ZStack {
                     Color(.orange)
                     Text("CREATE SCORECARD")
